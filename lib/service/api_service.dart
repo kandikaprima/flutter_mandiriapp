9a395302ac263f315d579b7e6bb999be5ff033ec
Kandika Prima Putra <kandikaprima@gmail.com>
2022-06-22 15:10:43 +0800

    change the endpoints

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -7,7 +7,7 @@ import 'package:flutter_mandiriapp/models/supplier_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  String endpoint = "https://6139-182-1-196-222.ap.ngrok.io/api/";
+  String endpoint = "https://9d30-182-1-213-74.ap.ngrok.io/api/";
 
   Future<AdminModel> login(String nip, String password) async {
     try {
