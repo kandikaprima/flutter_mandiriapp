0599e1dc99736584b502af21e14a3ce763ec8a32
Kandika Prima Putra <kandikaprima@gmail.com>
2022-06-22 12:14:03 +0800

    add const

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-Color blueMain = Color(0xFF003D79);
-Color yellowMain = Color(0xFFFFB700);
-Color redMain = Color(0xFFF01818);
+Color blueMain = const Color(0xFF003D79);
+Color yellowMain = const Color(0xFFFFB700);
+Color redMain = const Color(0xFFF01818);
